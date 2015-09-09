745dd72a3156e707d18994e4485c68bb033f9e90
Frank Duncan <frank.duncan@northwestern.edu>
2015-09-09 14:23:52 -0500

    wip

--- bin/buildosxrelease.sh
+++ bin/buildosxrelease.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-bin/buildosxexec.sh
-mkdir tmp
-
-cp -ap ../dist/osx/CLNL.app tmp
-cp osxsbcl tmp/CLNL.app/Contents/MacOS/
-cd tmp
-hdiutil create ../CLNL.dmg -srcfolder CLNL.app/
+bin/buildosxexec.sh && \
+mkdir tmp && \
+cp -ap dist/osx/CLNL.app tmp && \
+mv osxsbcl tmp/CLNL.app/Contents/MacOS/ && \
+cd tmp && \
+hdiutil create ../CLNL.dmg -srcfolder CLNL.app/ && \
 cd ..
 rm -rf tmp
